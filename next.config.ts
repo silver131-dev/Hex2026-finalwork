b4e793fa7c23128ab80e73e6d8b21a84d9e5854a
鄧小傳 <silver131@gmail.com>
2026-06-08 00:40:19 +0800

    fix: 設定 basePath 與關閉圖片優化解決破圖問題

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  //輸出純靜態檔案
+  output: "export",
+  
+  // GitHub 專案名稱 
+  basePath: "/Hex2026-finalwork",
+  
+  // 預設的圖片優化，讓靜態圖片可以直接顯示
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
